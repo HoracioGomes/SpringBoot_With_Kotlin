a456172ff7cff5648fbc990f420f1c177438a51a
Horacio <horacio.junior@ibm.com>
2023-04-28 17:25:25 -0300

    Changung json property

--- src/main/kotlin/br/com/teste/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/teste/data/vo/v1/PersonVO.kt
@@ -1,16 +1,20 @@
 package br.com.teste.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.Column
 
 data class PersonVO(
 
     var id: Long = 0,
 
+//    @field:JsonProperty("nickname")
     var firstName: String = "",
 
     var lastName: String = "",
 
     var address: String = "",
 
+//    @field:JsonIgnore
     var gender: String = ""
 )
