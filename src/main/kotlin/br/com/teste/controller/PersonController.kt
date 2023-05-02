4cc8487958038b8b62dc75555a6ab04ae5b413af
Horacio <horacio.junior@ibm.com>
2023-05-02 14:17:09 -0300

    Initial implementation of hateoas

--- src/main/kotlin/br/com/teste/controller/PersonController.kt
+++ src/main/kotlin/br/com/teste/controller/PersonController.kt
@@ -1,13 +1,13 @@
 package br.com.teste.controller
 
 import br.com.teste.data.vo.v1.PersonVO
-import br.com.teste.data.vo.v2.PersonVO as PersonVOV2
 import br.com.teste.mapper.DozerMapper
 import br.com.teste.mapper.custom.PersonMapper
 import br.com.teste.model.Person
 import br.com.teste.services.PersonService
 import br.com.teste.util.MediaType
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -25,7 +25,12 @@ class PersonController {
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YML_VALUE])
     fun findAll(): List<PersonVO> {
-        return DozerMapper().parseListObject(service.findAll(), PersonVO::class.java)
+        val listVO: List<PersonVO> = DozerMapper().parseListObject(service.findAll(), PersonVO::class.java)
+        for (personVO in listVO) {
+            val withSelfRel = linkTo(PersonController::class.java).slash(personVO.key).withSelfRel()
+            personVO.add(withSelfRel)
+        }
+        return listVO
     }
 
     @GetMapping(
@@ -33,7 +38,10 @@ class PersonController {
         produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YML_VALUE]
     )
     fun findById(@PathVariable(value = "id") id: Long): PersonVO {
-        return DozerMapper().parseObject(service.findById(id), PersonVO::class.java)
+        val personVO: PersonVO = DozerMapper().parseObject(service.findById(id), PersonVO::class.java)
+        val withSelfRel = linkTo(PersonController::class.java).slash(personVO.key).withSelfRel()
+        personVO.add(withSelfRel)
+        return personVO
     }
 
     @PostMapping(
@@ -43,19 +51,21 @@ class PersonController {
     )
     fun create(@RequestBody person: PersonVO): PersonVO {
         val entity: Person = DozerMapper().parseObject(person, Person::class.java)
-        return DozerMapper().parseObject(service.create(entity), PersonVO::class.java)
-
+        val personVO: PersonVO = DozerMapper().parseObject(service.create(entity), PersonVO::class.java)
+        val withSelfRel = linkTo(PersonController::class.java).slash(personVO.key).withSelfRel()
+        personVO.add(withSelfRel)
+        return personVO
     }
 
-    @PostMapping(
-        value = ["/v2"],
-        consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YML_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YML_VALUE]
-    )
-    fun create(@RequestBody person: PersonVOV2): PersonVOV2 {
-        val entity: Person = personMapper.parseToEntity(person)
-        return personMapper.parseToVO(service.create(entity))
-    }
+//    @PostMapping(
+//        value = ["/v2"],
+//        consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YML_VALUE],
+//        produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YML_VALUE]
+//    )
+//    fun create(@RequestBody person: PersonVOV2): PersonVOV2 {
+//        val entity: Person = personMapper.parseToEntity(person)
+//        return personMapper.parseToVO(service.create(entity))
+//    }
 
     @PutMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YML_VALUE],
@@ -63,7 +73,10 @@ class PersonController {
     )
     fun update(@RequestBody person: PersonVO): PersonVO {
         val entity: Person = DozerMapper().parseObject(person, Person::class.java)
-        return DozerMapper().parseObject(service.create(entity), PersonVO::class.java)
+        val personVO = DozerMapper().parseObject(service.create(entity), PersonVO::class.java)
+        val withSelfRel = linkTo(PersonController::class.java).slash(personVO.key).withSelfRel()
+        personVO.add(withSelfRel)
+        return personVO
     }
 
     @DeleteMapping(
--- src/main/kotlin/br/com/teste/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/teste/data/vo/v1/PersonVO.kt
@@ -2,11 +2,13 @@ package br.com.teste.data.vo.v1
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.github.dozermapper.core.Mapping
 import jakarta.persistence.Column
+import org.springframework.hateoas.RepresentationModel
 
 data class PersonVO(
-
-    var id: Long = 0,
+    @Mapping("id")
+    var key: Long = 0,
 
 //    @field:JsonProperty("nickname")
     var firstName: String = "",
@@ -17,4 +19,4 @@ data class PersonVO(
 
 //    @field:JsonIgnore
     var gender: String = ""
-)
+) : RepresentationModel<PersonVO>()
