dd332bd63f9aad293706572093a879e883d35367
Horacio <horacio.junior@ibm.com>
2023-05-03 10:15:50 -0300

    Field adjustments

--- src/main/kotlin/br/com/teste/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/teste/data/vo/v1/PersonVO.kt
@@ -2,12 +2,15 @@ package br.com.teste.data.vo.v1
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.github.dozermapper.core.Mapping
 import jakarta.persistence.Column
 import org.springframework.hateoas.RepresentationModel
 
+@JsonPropertyOrder("id", "firstName","lastName", "address", "gender")
 data class PersonVO(
     @Mapping("id")
+    @field:JsonProperty("id")
     var key: Long = 0,
 
 //    @field:JsonProperty("nickname")
